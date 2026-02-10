0b84aba94a296f4c4fe9f39533ac6cb9379c43c9
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-02-10 01:27:09 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Space Grotesk', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
