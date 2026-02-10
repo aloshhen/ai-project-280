b772d473c56ea5cf90703f00f99bcd29451f8baa
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-02-10 01:50:06 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,22 @@ export default {
       colors: {
         black: '#000000',
         white: '#FFFFFF',
-        gray: {
-          50: '#F9F9F9',
-          100: '#F3F3F3',
-          200: '#E5E5E5',
-          300: '#D4D4D4',
-          400: '#A3A3A3',
-          500: '#737373',
-          600: '#525252',
-          700: '#404040',
-          800: '#262626',
-          900: '#171717',
+      },
+      animation: {
+        'grain': 'grain 8s steps(10) infinite',
+      },
+      keyframes: {
+        grain: {
+          '0%, 100%': { transform: 'translate(0, 0)' },
+          '10%': { transform: 'translate(-5%, -10%)' },
+          '20%': { transform: 'translate(-15%, 5%)' },
+          '30%': { transform: 'translate(7%, -25%)' },
+          '40%': { transform: 'translate(-5%, 25%)' },
+          '50%': { transform: 'translate(-15%, 10%)' },
+          '60%': { transform: 'translate(15%, 0%)' },
+          '70%': { transform: 'translate(0%, 15%)' },
+          '80%': { transform: 'translate(3%, 35%)' },
+          '90%': { transform: 'translate(-10%, 10%)' },
         }
       }
     },
