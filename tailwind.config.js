85665b1b51cb0a9cf9fdcf310803852d98b58d62
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-02-10 01:32:00 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,24 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Space Grotesk', 'sans-serif'],
-        mono: ['JetBrains Mono', 'monospace'],
+        sans: ['Inter', 'sans-serif'],
       },
+      colors: {
+        black: '#000000',
+        white: '#FFFFFF',
+        gray: {
+          50: '#F9F9F9',
+          100: '#F3F3F3',
+          200: '#E5E5E5',
+          300: '#D4D4D4',
+          400: '#A3A3A3',
+          500: '#737373',
+          600: '#525252',
+          700: '#404040',
+          800: '#262626',
+          900: '#171717',
+        }
+      }
     },
   },
   plugins: [],
